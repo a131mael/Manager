6cf2ec4092ac46418b2d46192c656684c09bdef7
abimael <a131mael@gmail.com>
2016-05-20 13:21:49 -0300

    a

---
+++ UIWeb/src/main/java/org/aaf/auth/Realm.java
@@ -0,0 +1,78 @@
+package org.aaf.auth;
+
+
+import javax.inject.Inject;
+import javax.naming.NamingException;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+
+/**
+ *
+ * @author abimael
+ */
+public class Realm extends AuthorizingRealm {
+	
+//	@Inject
+//	private UserService userService;
+
+
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection arg0) {
+
+//        User usuario = (User) SecurityUtils.getSubject().getPrincipal();
+//
+//        SimpleAuthorizationInfo info = null;
+//        
+//        if (usuario != null) {
+//        	info = new SimpleAuthorizationInfo();
+//        }
+//
+//		return info;
+		return null;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken arg0) throws AuthenticationException {
+//		try {
+//			StringBuilder sb = new StringBuilder();
+//			
+//			for(char c : (char [])arg0.getCredentials()){
+//				sb.append(c);	
+//			}
+//			 
+//			System.out.println("Autorizacao");
+//			User m = new User();
+//			m.setLogin((String)arg0.getPrincipal());
+//			m.setSenha(sb.toString());
+//			userService = getEjb(UserService.class, UserService.class);
+//			User member = userService.login(m);
+//			if(member != null){
+//				return new SimpleAuthenticationInfo(member, m.getSenha(), getName());
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	
+		return null;
+	}
+
+//	@SuppressWarnings("rawtypes")
+//	private UserService getEjb(Class impl, Class local) {
+//		try {
+//			return ServiceLocator.getInstance().getEjb(impl.getSimpleName(), local.getName());
+//		} catch (NamingException e) {
+//			e.printStackTrace();
+//		}
+//		return userService;
+//	}
+
+}
---
+++ UIWeb/src/main/java/org/aaf/uiweb/controller/MatchController.java
@@ -0,0 +1,55 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.aaf.uiweb.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.inject.Model;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+@Model
+public class MatchController extends AuthController {
+
+	@Inject
+	private FacesContext facesContext;
+
+//	@Inject
+//	private MatchService matchService;
+//
+//	public List<MatchDTO> getTeamMatchs(int round) throws Exception {
+//		return matchService.getMatches(getLoggedUser().getId(),
+//				getLoggedUser().getTeam().getLeague().getCountry().getSession(), round);
+//
+//	}
+//
+//	public List<MatchDTO> getLastTeamMatchs() throws Exception {
+//		return matchService.getLastMatches(getLoggedUser().getId());
+//
+//	}
+
+	public List<Integer> getWeeks() throws Exception {
+		List<Integer> semanas = new ArrayList<Integer>();
+		for (int i = 0; i < 14; i++) {
+			semanas.add(i);
+		}
+		return semanas;
+
+	}
+
+}
